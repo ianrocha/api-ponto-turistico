384779326c861b79723a7e9e3888accc49e0c919
Ian Rocha <ianbritosr@gmail.com>
2018-07-06 10:51:37 -0300

    Add foto field to atracoes and pontos-turisticos

--- pontos_turisticos/urls.py
+++ pontos_turisticos/urls.py
@@ -17,6 +17,10 @@ from django.contrib import admin
 from django.conf.urls import include
 from django.urls import path 
 from rest_framework import routers
+
+from django.conf import settings
+from django.conf.urls.static import static
+
 from core.api.viewsets import PontoTuristicoViewSet
 from atracoes.api.viewsets import AtracaoViewSet
 from enderecos.api.viewsets import EnderecoViewSet
@@ -25,7 +29,7 @@ from avaliacoes.api.viewsets import AvaliacaoViewSet
 
 
 router = routers.DefaultRouter()
-router.register(r'pontoturistico', PontoTuristicoViewSet, base_name='PontoTuristico')
+router.register(r'pontosturisticos', PontoTuristicoViewSet, base_name='PontoTuristico')
 router.register(r'atracoes', AtracaoViewSet)
 router.register(r'enderecos', EnderecoViewSet)
 router.register(r'comentarios', ComentarioViewSet)
@@ -35,4 +39,4 @@ router.register(r'avaliacoes', AvaliacaoViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
