b9cf0500934b9e266b02205dcae7499960b3f099
Ian Rocha <ianbritosr@gmail.com>
2018-07-06 17:58:05 -0300

    improve permissions

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -1,7 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.filters import SearchFilter
 from rest_framework.decorators import action
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly, DjangoModelPermissions
 from rest_framework.authentication import TokenAuthentication
 from core.models import PontoTuristico
 from .serializers import PontoTuristicoSerializer
@@ -11,7 +11,7 @@ class PontoTuristicoViewSet(ModelViewSet):
     serializer_class = PontoTuristicoSerializer
     filter_backends = (SearchFilter,)
     search_fields = ('nome','endereco__linha1',)
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (DjangoModelPermissions,)
     authentication_classes = (TokenAuthentication,)
     # Muda argumento padrão de pesquisa
     # lookup_field = 'nome'
