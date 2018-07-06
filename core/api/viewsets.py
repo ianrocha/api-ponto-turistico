18a4b304b4f6e09d924ded7702742844a28b35e9
Ian Rocha <ianbritosr@gmail.com>
2018-07-06 14:11:52 -0300

    Add query-string filter example

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -11,6 +11,22 @@ class PontoTuristicoViewSet(ModelViewSet):
         return PontoTuristico.objects.filter(aprovado=True)
 
     def list(self, request, *args, **kwargs):
+        # Exemplo de filtro com query-string
+        # id = self.request.query_params.get('id', None)
+        # nome = self.request.query_params.get('nome', None)
+        # descricao = self.request.query_params.get('descricao', None)
+        # queryset = PontoTuristico.objects.all()
+
+        # if id:
+        #     queryset = queryset.objects.filter(pk=id)
+
+        # if nome:
+        #     queryset = queryset.objects.filter(nome__iexact=nome)
+
+        # if descricao:
+        #     queryset = queryset.objects.filter(descricao__iexact=descricao)
+
+        # return queryset
         return super(PontoTuristicoViewSet, self).list(request, *args, **kwargs)
     
     
