7b5d906b74282e356b73a2826cae5fe819d75780
Ian Rocha <ianbritosr@gmail.com>
2018-07-09 15:30:57 -0300

    Improved NestedRelationships in pontos-turisticos.serializers

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework.fields import SerializerMethodField
 from rest_framework.serializers import ModelSerializer
 from core.models import PontoTuristico
+from enderecos.models import Endereco
+from atracoes.models import Atracao
 from atracoes.api.serializers import AtracaoSerializer
 from enderecos.api.serializers import EnderecoSerializer
 from comentarios.api.serializers import ComentarioSerializer
@@ -9,18 +11,39 @@ from avaliacoes.api.serializers import AvaliacaoSerializer
 
 
 class PontoTuristicoSerializer(ModelSerializer):
+    comentarios = ComentarioSerializer(many=True, read_only=True)
+    avaliacoes = AvaliacaoSerializer(many=True, read_only=True)
     atracoes = AtracaoSerializer(many=True)
     endereco = EnderecoSerializer()
-    comentarios = ComentarioSerializer(many=True)
-    avaliacoes = AvaliacaoSerializer(many=True)
     desc_completa = SerializerMethodField()
 
     class Meta:
         model = PontoTuristico
         fields = ('id', 'nome', 'descricao', 'foto', 
-                  'aprovado', 'atracoes', 'comentarios', 
-                  'avaliacoes', 'endereco', 'desc_completa',
-                  'descricao_completa2')
+                  'aprovado', 'endereco', 'atracoes', 'comentarios', 
+                  'avaliacoes', 'desc_completa', 'descricao_completa2')
+        read_only_fields = ('atracoes', 'comentarios', 'avaliacoes')
+
+
+    def criar_endereco(self, endereco, ponto):
+        en = Endereco.objects.create(**endereco)
+        ponto.endereco = en
+
+    def criar_atracoes(self, atracoes, ponto):
+        for atracao in atracoes:
+            at = Atracao.objects.create(**atracao)
+            ponto.atracoes.add(at)
+
+    def create(self, validated_data):
+        endereco = validated_data['endereco']
+        atracoes = validated_data['atracoes']
+        del validated_data['endereco'], validated_data['atracoes']
+        ponto = PontoTuristico.objects.create(**validated_data)
+        self.criar_endereco(endereco, ponto)
+        self.criar_atracoes(atracoes, ponto)
+
+        return ponto
+    
 
     def get_desc_completa(self, obj):
         return '%s - %s' % (obj.nome, obj.descricao)
--- pontos_turisticos/settings.py
+++ pontos_turisticos/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['api-pontos-turisticos.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'api-pontos-turisticos.herokuapp.com']
 
 
 # Application definition
