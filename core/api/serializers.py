4ff3e441e32a5979a7503c19c7c811c287bd879a
Ian Rocha <ianbritosr@gmail.com>
2018-07-09 16:37:34 -0300

    Add OneToOne Relationship in pontos-turisticos

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework.fields import SerializerMethodField
 from rest_framework.serializers import ModelSerializer
-from core.models import PontoTuristico
+from core.models import PontoTuristico, DocIdentificacao
 from enderecos.models import Endereco
 from atracoes.models import Atracao
 from atracoes.api.serializers import AtracaoSerializer
@@ -9,19 +9,25 @@ from comentarios.api.serializers import ComentarioSerializer
 from avaliacoes.api.serializers import AvaliacaoSerializer
 
 
+class DocIdentificacaoSerializer(ModelSerializer):
+    class Meta:
+        model = DocIdentificacao
+        fields = '__all__'
 
 class PontoTuristicoSerializer(ModelSerializer):
     comentarios = ComentarioSerializer(many=True, read_only=True)
     avaliacoes = AvaliacaoSerializer(many=True, read_only=True)
     atracoes = AtracaoSerializer(many=True)
     endereco = EnderecoSerializer()
+    doc_identificacao = DocIdentificacaoSerializer()
     desc_completa = SerializerMethodField()
 
     class Meta:
         model = PontoTuristico
         fields = ('id', 'nome', 'descricao', 'foto', 
                   'aprovado', 'endereco', 'atracoes', 'comentarios', 
-                  'avaliacoes', 'desc_completa', 'descricao_completa2')
+                  'avaliacoes', 'desc_completa', 'descricao_completa2',
+                  'doc_identificacao')
         read_only_fields = ('atracoes', 'comentarios', 'avaliacoes')
 
 
@@ -36,11 +42,22 @@ class PontoTuristicoSerializer(ModelSerializer):
 
     def create(self, validated_data):
         endereco = validated_data['endereco']
+        del validated_data['endereco']
+
         atracoes = validated_data['atracoes']
-        del validated_data['endereco'], validated_data['atracoes']
+        del validated_data['atracoes']
+
+        doc = validated_data['doc_identificacao']
+        del validated_data['doc_identificacao']
+        doci = DocIdentificacao.objects.create(**doc)
+
         ponto = PontoTuristico.objects.create(**validated_data)
+        
         self.criar_endereco(endereco, ponto)
         self.criar_atracoes(atracoes, ponto)
+        ponto.doc_identificacao = doci
+
+        ponto.save()
 
         return ponto
     
